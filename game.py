57885896ace50256c9912cedf315d6d30807ebd5
LegoManACM <41910060+LegoManACM@users.noreply.github.com>
2025-11-30 21:23:55 -0800

    Updated UI and game logic

--- game.py
+++ game.py
@@ -6,6 +6,9 @@ from miningActionDrill import MiningActionDrill
 from miningActionBlast import MiningActionBlast
 from miningActionScan import MiningActionScan
 
+from time import sleep
+import os
+
 class Game:
 
     def setupGame(self):
@@ -34,41 +37,86 @@ class Game:
         self.gameLoopState = 0
 
     def presentMiningLayers(self):
+        toReturn = ""
         for layer in self.asteroid.layers:
-            print("[" + layer.getPrintout() + "]")
+            toReturn += ("[" + layer.getPrintout() + "]")
+            toReturn += "\n"
+        return(toReturn)
 
     def presentMiningActions(self):
-        print("\nAvailible Actions")
+        toReturn = ""
+        toReturn += "Availible Actions"
         for i in range(0, len(self.actions)):
-            print(">>-> " + str(i) + " " + self.actions[i].name)
+            toReturn += ("\n>>-> " + str(i) + " " + self.actions[i].name)
+        return(toReturn)
+
+    def presentMiningMenu(self):
+        linesLayers = self.presentMiningLayers().splitlines()
+        linesActions = self.presentMiningActions().splitlines()
+
+        toPrint = ""
+
+        rows = len(linesLayers)
+        if(len(linesActions) > rows):
+            rows = len(linesActions)
+        
+        for i in range(0, rows):
+            toPrint += "|  "
+            if(i < len(linesLayers)):
+                toPrint += linesLayers[i]
+            toPrint += "  |  "
+            if(i < len(linesActions)):
+                toPrint += linesActions[i]
+            toPrint += "\n"
+
+        print(toPrint)
+
+
+    def inputErrorMessage(self):
+        os.system('cls')
+        print("\n\n\n")
+        print("[Please enter an integer 0 - " + str(len(self.actions) - 1) + "]")
+        sleep(5)
 
     def doActionLoop(self):
         cInput = ""
         deliberating = True
         while deliberating:
-            self.presentMiningActions()
+            os.system('cls')
+            self.presentMiningMenu()
             cInput = input("\n...")
-            if(int(cInput) != None):
-                if(int(cInput) < len(self.actions) and int(cInput) >= 0):
-                    deliberating = False
-                    cInput = int(cInput)
-                else:
-                    print("enter a number between 0 and " + str(len(self.actions) - 1))
+            if(cInput.isdigit()):
+                if(int(cInput) != None):
+                    if(int(cInput) < len(self.actions) and int(cInput) >= 0):
+                        deliberating = False
+                        cInput = int(cInput)
+                    else:
+                        self.inputErrorMessage()
             else:
-                print("please enter an integer")
+                self.inputErrorMessage()
                 
         self.asteroid.layers = self.actions[cInput].use(self)
 
 
     def doGameLoop(self):
         if(self.gameLoopState == 0):   # choose mining action
-            self.presentMiningLayers()
+            os.system('cls')
             self.gameLoopState += 1     
         elif(self.gameLoopState == 1): # collect resources
             self.doActionLoop()    
-            self.gameLoopState = 0
+            self.gameLoopState = 2
         elif(self.gameLoopState == 2): # wrap-up
-            pass
+            first = self.asteroid.getFirstSolid()
+            if(first):
+                self.asteroid.layers[first].known = True
+                self.asteroid.layers[first].update()
+                self.gameLoopState = 0
+            else:
+                self.gameLoopState = 3
+        elif(self.gameLoopState == 3):
+            print("Asteroid complete")
+            sleep(5)
+            print(str(1/0))
 
     def playGame(self):
         playing = True
